db7a8f609a88fa91c6f21ad764866e611cab0e70
msinummoc <msinummoc@126.com>
2025-12-23 16:31:28 +0800

    123

---
+++ selfmade2.mod
@@ -0,0 +1,4 @@
+name="selfmade2"
+path="mod/selfmade2"
+
+supported_version="1.37.*"
---
+++ selfmade2/common/defines/selfmade.lua
@@ -0,0 +1,9 @@
+
+
+
+--NDefines.NEconomy.DECREASE_AUTONOMY_STEP = -10
+--NDefines.NEconomy.DECREASE_AUTONOMY_MIN = 5
+--NDefines.NEconomy.AUTONOMY_CHANGE_DURATION = 0
+
+--NDefines.NNationDesigner.MAX_GOVERNMENT_RANK = 4  --3 
+--NDefines.NCountry.MAX_GOV_RANK = 4 -- 3
